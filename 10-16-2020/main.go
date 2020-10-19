a0c51aec01c2ed11b3aa3d229ee4c23e51bd0ec8
Ian Pratt <thingcoder1@gmail.com>
2020-10-19 14:28:03 -0500

    rolling back changes

--- 10-16-2020/main.go
+++ 10-16-2020/main.go
@@ -9,5 +9,5 @@ n:=make([][]int,len(r))
 for i:=0;i<len(r);i++{for j:=0;j<len(r[i]);j++{if r[i][j]!=""{t,_:=strconv.Atoi(r[i][j])
 n[i]=append(n[i],t)}}}
 for i:=0;i<len(n);i++{for range n[i]{x:=n[i]
-for k:=range x{if k<len(x)-1&&x[k]>x[k+1]{n[i][k],n[i][k+1]=n[i][k+1],n[i][k]}}}}
+for k:=range x{if k<len(x)-1&&x[k]>x[k+1]{n[i][k],n[i][k+1]=x[k+1],x[k]}}}}
 fmt.Printf("%v\n",n)}
