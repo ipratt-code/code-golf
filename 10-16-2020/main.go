e29bdfb4953faa56be091dfba83a687d01565c82
Ian Pratt <thingcoder1@gmail.com>
2020-10-19 16:16:32 -0500

    even less characters

--- 10-16-2020/main.go
+++ 10-16-2020/main.go
@@ -6,8 +6,8 @@ import("encoding/csv"
 func main(){c,_:=os.Open(".csv")
 r,_:=csv.NewReader(c).ReadAll()
 n:=make([][]int,len(r))
-for i:=0;i<len(r);i++{q:=r[i]
-for j:=0;j<len(q);j++{if q[j]!=""{t,_:=strconv.Atoi(q[j])
+for i:=range r{q:=r[i]
+for j:=range q{if q[j]!=""{t,_:=strconv.Atoi(q[j])
 n[i]=append(n[i],t)}}}
 for i:=range n{x:=n[i]
 for range x{for k:=0;k<len(x)-1;k++{if x[k]>x[k+1]{n[i][k],n[i][k+1]=x[k+1],x[k]}}}}
