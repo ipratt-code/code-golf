d3428fb87fff6ba5b17e981c25035e5a9f55cf12
Ian Pratt <thingcoder1@gmail.com>
2020-10-19 11:03:12 -0500

    remove more whitespace

--- 10-16-2020/main.go
+++ 10-16-2020/main.go
@@ -3,19 +3,19 @@ import ("encoding/csv"
 "fmt"
 "os"
 "strconv")
-func main() {
+func main(){
 c,_:=os.Open(".csv")
 r,_:=csv.NewReader(c).ReadAll()
 n:=make([][]int,len(r))
-for i:=0;i<len(r);i++ {
-for j:=0;j<len(r[i]);j++ {
-if r[i][j]!="" {
+for i:=0;i<len(r);i++{
+for j:=0;j<len(r[i]);j++{
+if r[i][j]!=""{
 t,_:=strconv.Atoi(r[i][j])
-n[i]=append(n[i], t)}}}
-for i:= 0;i<len(n);i++ {
-for h:=0;h<len(n[i]);h++ {
-for k:=0;k<len(n[i])-1;k++ {
-if n[i][k]>n[i][k+1] {
+n[i]=append(n[i],t)}}}
+for i:=0;i<len(n);i++{
+for h:=0;h<len(n[i]);h++{
+for k:=0;k<len(n[i])-1;k++{
+if n[i][k]>n[i][k+1]{
 x:=n[i][k]
 n[i][k]=n[i][k+1]
 n[i][k+1]=x}}}}
