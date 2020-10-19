39211ff3ad117cf4cabe621e7d2ce4991d4a375b
Ian Pratt <thingcoder1@gmail.com>
2020-10-19 10:26:12 -0500

    made the golf hope it works!

--- 10-16-2020/main.go
+++ 10-16-2020/main.go
@@ -1,13 +1,35 @@
 package main
 
 import (
-	"os"
 	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
 )
 
-func main(){
-	c,_ := os.Open(".csv")
-	r,_ := csv.NewReader(c).ReadAll()
+func main() {
+	c, _ := os.Open(".csv")
+	r, _ := csv.NewReader(c).ReadAll()
 	n := make([][]int, len(r))
-	print(n)
+	for i := 0; i < len(r); i++ {
+		for j := 0; j < len(r[i]); j++ {
+			if r[i][j] != "" {
+				t, _ := strconv.Atoi(r[i][j])
+				n[i] = append(n[i], t)
+			}
+		}
+	}
+	fmt.Printf("%v\n", n)
+	for i := 0; i < len(n); i++ {
+		for h := 0; h < len(n[i]); h++ {
+			for k := 0; k < len(n[i])-1; k++ {
+				if n[i][k] > n[i][k+1] {
+					x := n[i][k]
+					n[i][k] = n[i][k+1]
+					n[i][k+1] = x
+				}
+			}
+		}
+	}
+	fmt.Printf("%v\n", n)
 }
